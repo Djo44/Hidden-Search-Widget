c399faac47ae42efa5bbbfed2cfb1abc726a5d04
unknown <djordjetelekom@gmail.com>
2021-08-18 22:29:00 +0200

    search widget

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", () => {
+  container.classList.toggle("active");
+  input.focus();
+});
